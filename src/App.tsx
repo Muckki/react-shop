da454c02760f90e2830f57aaa740f1d49ad2ffa5
Muckki <ckdduq0508@gmail.com>
2023-05-16 00:15:10 +0900

    commits

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
        </div>
        <div className="fassion-line">
         <h2>패션</h2>
-        <div className="fassion container">
+        <div className="fassions container">
           <div className="fassion-item1"></div>
           <div className="fassion-item2"></div>
           <div className="fassion-item3"></div>
@@ -48,19 +48,19 @@ function App() {
         </div>
        </div>
        <footer className='bottom-line'>
-        <div className="class-name"></div>
+        <div className="class-name">제로베이스</div>
         <div className="cards">
-          <div className="card1"></div>
-          <div className="card2"></div>
-          <div className="card3"></div>
-          <div className="card4"></div>
-          <div className="card5"></div>
-          <div className="card6"></div>
+          <div className="card1">비자</div>
+          <div className="card2">마스터</div>
+          <div className="card3">아멕스</div>
+          <div className="card4">니들은</div>
+          <div className="card5">뭔지를</div>
+          <div className="card6">모르겠는데</div>
         </div>
         <div className="snss">
-          <div className="facebook"></div>
-          <div className="instagram"></div>
-          <div className="github"></div>
+          <div className="facebook">페북</div>
+          <div className="instagram">인스타</div>
+          <div className="github">깃허브</div>
         </div>
         <div className="copyrights">Copyright © 2023 Zero Base</div>
        </footer>
