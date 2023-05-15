5aed7f7a90d1cef18716ebec7f486b0b075fe6ff
Muckki <ckdduq0508@gmail.com>
2023-05-15 23:57:00 +0900

    4 commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,55 @@ function App() {
   return (
     <>
       <div className='react-shop'>
-       놀랐어? 보고싶어
+       <header className='head-line'>
+        <div className="home button"></div>
+        <div className="fassion button"></div>
+        <div className="accs button"></div>
+        <div className="digital button"></div>
+        <div className="light button"></div>
+        <div className="search input"></div>
+        <div className="shopping button"></div>
+       </header>
+       <div className="carausel">
+        <div className="jean inner"></div>
+        <div className="digit inner"></div>
+        <div className="foods inner"></div>
+       </div>
+       <div className="fassion-line">
+        <div className="fassion-item1"></div>
+        <div className="fassion-item2"></div>
+        <div className="fassion-item3"></div>
+        <div className="fassion-item4"></div>
+       </div>
+       <div className="accs-line">
+        <div className="accs-item1"></div>
+        <div className="accs-item2"></div>
+        <div className="accs-item3"></div>
+        <div className="accs-item4"></div>
+       </div>
+       <div className="digital-line">
+        <div className="digital-item1"></div>
+        <div className="digital-item2"></div>
+        <div className="digital-item3"></div>
+        <div className="digital-item4"></div>
+       </div>
+       <footer className='bottom-line'>
+        <div className="class-name"></div>
+        <div className="cards">
+          <div className="card1"></div>
+          <div className="card2"></div>
+          <div className="card3"></div>
+          <div className="card4"></div>
+          <div className="card5"></div>
+          <div className="card6"></div>
+        </div>
+        <div className="snss">
+          <div className="facebook"></div>
+          <div className="instagram"></div>
+          <div className="github"></div>
+        </div>
+        <div className="copyrights">Copyright © 2023 Zero Base</div>
+       </footer>
       </div>
      
     </>
