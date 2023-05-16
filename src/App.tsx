77905bf9bcdec01e9b2a99a7cf7fd0b3cb378d97
Muckki <ckdduq0508@gmail.com>
2023-05-16 22:51:25 +0900

    commits

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@
 import './App.css'
 
 function App() {
+  
+  const handleClick = (e:any) => {
+    e.preventDefault();
+  }
+ 
 
   return (
     <>
@@ -9,14 +14,14 @@ function App() {
        <header className='head-line'>
         <nav className='navigation-bar'>
           <div className="nav-container">
-            <a href='/' className="home button">React Shop</a>
+            <a href='/' className="home" onClick={handleClick}>React Shop</a>
             <div className="navi-flex">
               <div className="fassion button">패션</div>
               <div className="accs button">액세서리</div>
               <div className="digital button">디지털</div>
             </div>
             <div className="nav-flex">
-              <div className="light button">해</div>
+              <div className="light-switch">해</div>
               <div className="search input">검색</div>
               <div className="shopping button">장바구니
                 <div className="counting">0</div>
